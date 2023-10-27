52beece77d955579789e339f80ca046606f497c0
David Schneider <mail@schnav.id>
2023-10-27 18:34:12 +0200

    wgpu renderer clear screen

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -3,23 +3,32 @@ use std::time::Instant;
 
 use simple_logger::SimpleLogger;
 use winit::event::{Event, WindowEvent};
+use winit::window::Window;
 use winit::{dpi::LogicalSize, event_loop::EventLoop, window::WindowBuilder};
 
+use crate::render::{Renderer, WgpuRenderer};
 use crate::{core::filesystem::Filesystem, engine::ext::WindowExt};
-use crate::render::{GlRenderer, Renderer};
 
 pub mod ext;
 pub mod splash;
 
 pub fn entry_point() -> anyhow::Result<()> {
     SimpleLogger::new()
-        .with_level(log::LevelFilter::Debug)
+        .with_level(log::LevelFilter::Info)
         // .with_module_level("xray_oxide::core::filesystem", log::LevelFilter::Trace)
         .init()?;
 
     let start = Instant::now();
 
     let mut event_loop = EventLoop::new()?;
+
+    let window = WindowBuilder::new()
+        .with_title("XRay Oxide")
+        .with_inner_size(LogicalSize::new(1920.0, 1080.0))
+        .with_resizable(false)
+        .with_visible(false)
+        .build(&event_loop)
+        .unwrap();
 
     let prepare_thread = {
         let proxy = event_loop.create_proxy();
@@ -36,7 +45,7 @@ pub fn entry_point() -> anyhow::Result<()> {
                 ));
             }
 
-            let app = XRay::new();
+            let app = XRay::new(window);
             proxy.send_event(()).unwrap();
             app
         })
@@ -50,26 +59,35 @@ pub fn entry_point() -> anyhow::Result<()> {
 
     log::debug!("Created Application in {} seconds", duration.as_secs_f64());
 
-    let window = WindowBuilder::new()
-        .with_title("XRay Oxide")
-        .with_inner_size(LogicalSize::new(1920.0, 1080.0))
-        .with_resizable(false)
-        .with_visible(false)
-        .build(&event_loop)
-        .unwrap();
-
-    window.center_window(&event_loop);
-    window.set_visible(true);
-    window.focus_window();
+    {
+        let window = xray.renderer.window();
+        window.center_window(&event_loop);
+        window.set_visible(true);
+        window.focus_window();
+    }
 
     event_loop.run(move |event, target| {
         if let Event::WindowEvent { event, window_id } = event {
-            if window_id == window.id() {
+            if window_id == xray.renderer.window().id() {
                 match event {
                     WindowEvent::CloseRequested => {
                         target.exit();
                     }
-                    WindowEvent::RedrawRequested => {}
+                    WindowEvent::RedrawRequested => match xray.renderer.render() {
+                        Ok(_) => {}
+                        Err(e) => match e.downcast_ref::<wgpu::SurfaceError>() {
+                            Some(wgpu::SurfaceError::Lost) => xray.renderer.resize(None),
+                            Some(wgpu::SurfaceError::OutOfMemory) => {
+                                eprintln!("Out of Memory!");
+                                target.exit()
+                            }
+                            Some(e) => eprintln!("{e:?}"),
+                            None => eprintln!("{e:?}"),
+                        },
+                    },
+                    WindowEvent::Resized(new_size) => {
+                        xray.renderer.resize(Some(new_size));
+                    }
                     _ => {}
                 }
             }
@@ -96,7 +114,7 @@ pub struct XRay {
 }
 
 impl XRay {
-    pub fn new() -> anyhow::Result<XRay> {
+    pub fn new(window: Window) -> anyhow::Result<XRay> {
         let mut app = XRay {
             loaded: false,
             ll_dwReference: 0,
@@ -105,7 +123,7 @@ impl XRay {
             current_level: None,
             loading_screen: None,
             filesystem: Filesystem::new()?,
-            renderer: select_renderer()?,
+            renderer: select_renderer(window)?,
         };
 
         app.level_scan();
@@ -118,6 +136,8 @@ impl XRay {
     }
 }
 
-fn select_renderer() -> anyhow::Result<Box<dyn Renderer + Send>> {
-    Ok(Box::new(GlRenderer::new()?))
+fn select_renderer(window: Window) -> anyhow::Result<Box<dyn Renderer + Send>> {
+    let renderer = Box::new(pollster::block_on(WgpuRenderer::new(window))?);
+
+    Ok(renderer)
 }
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,15 +1,146 @@
+use thiserror::Error;
+use winit::dpi::PhysicalSize;
+use winit::window::Window;
+
 pub trait Renderer {
-
+    fn window(&self) -> &Window;
+    fn resize(&mut self, new_size: Option<PhysicalSize<u32>>);
+    fn render(&mut self) -> anyhow::Result<()>;
 }
 
-pub struct GlRenderer {
-
+#[derive(Debug, Error)]
+pub enum RendererError {
+    #[error("No compatible GPU found")]
+    NoGPUFound,
 }
 
-impl GlRenderer {
-    pub fn new() -> anyhow::Result<GlRenderer> {
-        Ok(GlRenderer {})
+pub struct WgpuRenderer {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: PhysicalSize<u32>,
+    window: Window,
+}
+
+impl WgpuRenderer {
+    pub async fn new(window: Window) -> anyhow::Result<WgpuRenderer> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            flags: Default::default(),
+            dx12_shader_compiler: Default::default(),
+            gles_minor_version: Default::default(),
+        });
+
+        let surface = unsafe { instance.create_surface(&window)? };
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .ok_or(RendererError::NoGPUFound)?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .copied()
+            .find(|format| format.is_srgb())
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        Ok(WgpuRenderer {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        })
     }
 }
 
-impl Renderer for GlRenderer {}
+impl Renderer for WgpuRenderer {
+    fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: Option<PhysicalSize<u32>>) {
+        let new_size = new_size.unwrap_or(self.size);
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn render(&mut self) -> anyhow::Result<()> {
+        let output = self.surface.get_current_texture()?;
+
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
